ffbdd98c57d5f73d187c54466e91964d89954fe7
Eugene Jun <genenamu@gmail.com>
2023-04-04 22:09:31 +0900

    feat: Update code

--- school_assignment/ch.5/5-4.py
+++ school_assignment/ch.5/5-4.py
@@ -1,12 +1,14 @@
 n = list(range(1, 10001))
-
+set_num = set()
 for i in range(1, 10001):
     d = i
     i_list = list(str(i))
     for j in i_list:
         d += int(j)
     if d <= 10000:
-        n.remove(d)
+        set_num.add(d)
 
+for i in list(set_num):
+    n.remove(i)
 for k in n:
-    print(n)
+    print(k)
