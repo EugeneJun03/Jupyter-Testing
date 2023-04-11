ed29c97f0adeec1fdd9d49aff008c80b278a625c
Eugene Jun <genenamu@gmail.com>
2023-04-11 14:02:45 +0900

    feat: Solved problem

--- school_assignment/ch.6/6-3.py
+++ school_assignment/ch.6/6-3.py
@@ -1,6 +1,31 @@
 import csv
+
+# 3-1
 f = open("average-latitude-longitude-countries.csv", "r")
 rdr = csv.reader(f)
+list1 = []
 for line in rdr:
-    print(line)
-f.close
+    list1.append((line[0], line[1]))
+f.close()
+#print(list1)
+
+f = open("average-latitude-longitude-countries.csv", "r")
+rdr = csv.reader(f)
+list2 = []
+for line in rdr:
+    if line[0] != "ISO 3166 Country Code":
+        list2.append((line[0], (float(line[2]), float(line[3]))))
+f.close()
+#print(list2)
+
+
+#3-2
+f = open("average-latitude-longitude-countries.csv", "r")
+rdr = csv.reader(f)
+n = input()
+for line in rdr:
+    if line[0] == n:
+        print(line[1])
+        break
+f.close()
+
